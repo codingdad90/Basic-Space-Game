86a321091c52d96c590b2755e56186e0b21c46ed
Maurice Aufderheide <mauriceaufderheide@icloud.com>
2022-09-20 14:29:29 -0400

    basic Menu created and converted to function

--- Commandlinetester/Program.cs
+++ Commandlinetester/Program.cs
@@ -3,6 +3,30 @@ using Gamerules;
 
 namespace Commandlinetester
 {
+
+    class CommandLineFunctions
+    {
+        static public string PeacefulSystem(string i)
+        {
+            Console.WriteLine("Welcome to [insert solar system]");
+            Console.WriteLine("Type 1 to visit the merchant, 2 to visit the colony, and 3 to visit the shipwright");
+             i = Console.ReadLine().ToString();
+            if (i == "1")
+            {
+                Console.WriteLine("Welcome to The Merchant");
+
+            }
+            else if (i == "2")
+            {
+                Console.WriteLine("Welcome to the Colony");
+            }
+            else if (i == "3")
+            {
+                Console.WriteLine("Welcome to the Shipwright");
+            }
+            return i;
+        }
+    }
     class Program
     {
          //string tempString;
@@ -19,12 +43,35 @@ namespace Commandlinetester
                 Console.WriteLine("Please enter a number!");
                 x = Console.ReadLine();
                 Console.WriteLine("You have chosen " + x);
+                string i = "0";
+                while( i!="x")
+                        {
+                   i= CommandLineFunctions.PeacefulSystem(i);
+                    
+                    /*Console.WriteLine("Welcome to [insert solar system]");
+                    Console.WriteLine("Type 1 to visit the merchant, 2 to visit the colony, and 3 to visit the shipwright");
+                    i = Console.ReadLine().ToString();
+                    if (i == "1")
+                    {
+                        Console.WriteLine("Welcome to The Merchant");
+
+                    }
+                    else if (i == "2")
+                    {
+                        Console.WriteLine("Welcome to the Colony");
+                    }
+                    else if (i == "3")
+                    {
+                        Console.WriteLine("Welcome to the Shipwright");
+                    } */
+
+                }
 
                 string[] NPCNames = System.IO.File.ReadAllLines("C:\\Users\\mauri\\source\\repos\\Basic Space Game\\Commandlinetester\\namelist.txt");
-                foreach (var names in NPCNames)
+                /*foreach (var names in NPCNames)
                 {
                     Console.WriteLine(names);
-                }
+                }*/
             }
 
         }
